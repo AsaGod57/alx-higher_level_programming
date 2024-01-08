0c74a742e7d58d898513665b24fb50ea51e6b15c
Godsway <gasamoah@wacci.ug.edu.gh>
2024-01-07 17:43:39 -0800

    My python matrix commit

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# 6-print_matrix_integer.py
+# Godsway Asamoah <gasamoah@wacci.ug.edu.gh>
+
+
+def print_matrix_integer(matrix=[[]]):
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+                print("{:d}".format(matrix[i][j]), end="")
+                if j != (len(matrix[i]) - 1):
+                    print(" ", end="")
+
+        print("")
