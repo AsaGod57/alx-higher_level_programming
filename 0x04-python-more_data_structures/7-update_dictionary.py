857ac6296d786bf50ba6b9c6f3cb1d0d292789da
Godsway <gasamoah@wacci.ug.edu.gh>
2024-01-10 16:21:28 -0800

    My python dictionary update commit

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 7-update_dictionary.py
+# Godsway Asamoah <gasamoah@wacci.ug.edu.gh>
+
+
+def update_dictionary(a_dictionary, key, value):
+
+    a_dictionary[key] = value
+    return (a_dictionary)
